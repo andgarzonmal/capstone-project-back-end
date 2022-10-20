a29cd7f007d821ff9655080a6bb0cd5f01453734
andgarzonmal <andgarzonmal@unal.edu.co>
2022-10-20 12:55:51 -0500

    Add first steps jwt config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'users#index'
-  devise_for :users, path: ''
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
