799624e11c786edb5f2e157894a19186503ef290
dqarias <dino.quispea@gmail.com>
2022-10-21 17:01:49 -0500

    Update session and fix response

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,10 +4,17 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
+    if current_user
     render json: {
       status: { code: 200, message: 'Logged in sucessfully.' },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
+    else 
+      render json: {
+        status: 401,
+        message: "Wrong email or password"
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
