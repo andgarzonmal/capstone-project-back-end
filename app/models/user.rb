1b13f500180bc6b0f196a86c5cbed738826d254c
dqarias <dino.quispea@gmail.com>
2022-10-21 12:13:24 -0500

    Add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :reservations
-  has_many :rooms, through: :reservations
+  has_many :reservations, dependent: :delete_all
+  has_many :rooms, through: :reservations, dependent: :delete_all
+
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
